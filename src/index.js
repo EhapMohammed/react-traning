064664a65587f3edf04423c38786c36f2bac071f
Your Name <ehab.hashem@almotahida.local>
2024-08-14 12:30:00 +0300

    moduls remove

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-import Dash_home from './dashboard/Dash_home.js';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dash_home />
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
